e422fbd8ec3de44f38403d8690306fab8ab72bfc
joyfive <joy_five@kakao.com>
2022-10-31 08:58:44 +0900

    instance Final

--- src/shared/Apis.js
+++ src/shared/Apis.js
@@ -1,69 +1,98 @@
 import axios from "axios"
 import { Cookies } from "react-cookie"
 
+// 인스턴스 3개 나누기 가능
 const cookies = new Cookies()
-
-const instance = axios.create({
-  noheader: {
-    baseURL: "${process.env.REACT_APP_API_URL}",
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-      accept: "application/json",
-    },
-    withCredentials: true,
-  },
+const notoken = axios.create({
+  baseURL: "process.env.REACT_APP_API_URL",
   headers: {
-    baseURL: "${process.env.REACT_APP_API_URL}",
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-      accept: "application/json",
-      Access_Token: `Bearer ${cookies.get("token")}`,
-      "Access-Control-Allow-Origin": "*",
-    },
-    withCredentials: true,
+    "content-type": "application/json;charset=UTF-8",
+    accept: "application/json",
   },
-  file: {
-    baseURL: "${process.env.REACT_APP_API_URL}",
-    headers: {
-      enctype: "multipart/form-data",
-      "content-type": "application/json;charset=UTF-8",
-      Access_Token: `Bearer ${cookies.get("token")}`,
-      accept: "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    withCredentials: true,
-  },
+  withCredentials: true,
 })
+
+const token = axios.create({
+  baseURL: "process.env.REACT_APP_API_URL",
+  headers: {
+    "content-type": "application/json;charset=UTF-8",
+    accept: "application/json",
+    Access_Token: `Bearer ${cookies.get("token")}`,
+    "Access-Control-Allow-Origin": "*",
+  },
+  withCredentials: true,
+})
+
+const file = axios.create({
+  baseURL: process.env.REACT_APP_URL,
+  headers: {
+    enctype: "multipart/form-data",
+
+    // Access_Token: `Bearer ${cookies.get("token")}`,
+    Access_Token: `eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZWg1MTY0IiwiZXhwIjoxNjY3MTM2MDQ2LCJpYXQiOjE2NjcxMzQyNDZ9.JBTAfVvxd8Fj7jn5gEUakygaEUgJdG0Wb-KjS_Y6CLw`,
+  },
+  withCredentials: true,
+})
+
+// // 객체는 안됨ㅋㅋㅋㅋ
+// const instance = axios.create({
+//   noheader: {
+//     baseURL: process.env.REACT_APP_API_URL,
+//     headers: {
+//       "content-type": "application/json;charset=UTF-8",
+//       accept: "application/json",
+//     },
+//     withCredentials: true,
+//   },
+//   headers: {
+//     baseURL: process.env.REACT_APP_API_URL,
+//     headers: {
+//       "content-type": "application/json;charset=UTF-8",
+//       accept: "application/json",
+//       Access_Token: `Bearer ${cookies.get("token")}`,
+//       "Access-Control-Allow-Origin": "*",
+//     },
+//     withCredentials: true,
+//   },
+//   file: {
+//     baseURL: process.env.REACT_APP_URL,
+//     headers: {
+//       enctype: "multipart/form-data",
+
+//       // Access_Token: `Bearer ${cookies.get("token")}`,
+//       Access_Token: `eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZWg1MTY0IiwiZXhwIjoxNjY3MTMzNzc3LCJpYXQiOjE2NjcxMzE5Nzd9.JbNXd29Cqy0PO1dBN2VPVFyfluf2H8YQZNIisR7AqbE`,
+//     },
+//     withCredentials: true,
+//   },
+// })
 
 // 예시
 export const Apis = {
   //로그인
-  loginAX: (loginInfo) =>
-    instance.noheader.post(`/team01/member/login`, loginInfo),
-  //회원가입
-  signupAX: (signupInfo) =>
-    instance.noheader.post(`/team01/member/signup`, signupInfo),
-  //이메일중복확인
-  usernameAX: (userid) =>
-    instance.noheader.post(`/team01/member/idCheck`, userid),
+  // loginAX: (loginInfo) => notoken.post(`/team01/member/login`, loginInfo),
+  // //회원가입
+  // signupAX: (signupInfo) =>
+  //   instance.nh.post(`/team01/member/signup`, signupInfo),
+  // //이메일중복확인
+  // usernameAX: (userid) => notoken.post(`/team01/member/idCheck`, userid),
   //게시글 작성
-  filePostAX: (postInfo) => instance.file.post(`/team01/post`, postInfo),
-  //게시글 수정
-  putPostAX: (postInfo) =>
-    instance.file.put(`/team/01/post/${postId}`, postInfo),
-  //게시글 삭제
-  deletePostAX: (id) => instance.headers.delete(`/team/01/post/${id}`),
-  //게시글 좋아요
-  likePostAX: (id) => instance.headers.get(`/team01/likes/${id}`),
-  //게시글 전체 조회
-  getPostAX: () => instance.headers.get(`/team01/getPost`),
-  //게시글 상세 조회
-  getDetailAX: () => instance.headers.get(`/team01/getPost/${postId}`),
+  filePostAX: (payload) => file.post(`/team01/post`, payload),
+  // //게시글 수정
+  // putPostAX: (postInfo) =>
+  //   file.put(`/team/01/post/${postInfo.postId}`, postInfo),
+  // //게시글 삭제
+  // deletePostAX: (id) => token.delete(`/team/01/post/${id}`),
+  // //게시글 좋아요
+  // likePostAX: (id) => token.get(`/team01/likes/${id}`),
+  // //게시글 전체 조회
+  // getPostAX: () => token.get(`/team01/getPost`),
+  // //게시글 상세 조회
+  // getDetailAX: (postInfo) => token.get(`/team01/getPost/${PostInfo.postId}`),
 
-  //댓글 작성
-  postCmtAX: (id) => instance.headers.post(`/team01/comment/${id}`),
-  //댓글 삭제
-  deleteCmtAX: (id) => instance.headers.post(`/team01/comment/${id}`),
+  // //댓글 작성
+  // postCmtAX: (id) => token.post(`/team01/comment/${id}`),
+  // //댓글 삭제
+  // deleteCmtAX: (id) => token.post(`/team01/comment/${id}`),
 }
 
 export default Apis
