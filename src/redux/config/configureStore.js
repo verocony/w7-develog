908185bfe452437d04bc8eff0045db91abb96b39
joyfive <joy_five@kakao.com>
2022-11-03 17:12:32 +0900

    merge

--- src/redux/config/configureStore.js
+++ src/redux/config/configureStore.js
@@ -6,29 +6,22 @@ import cmt from "../modules/cmtSlice"
 import posts from "../modules/postSilice2"
 import mylist from "../modules/mypageSlice"
 import search from "../modules/searchSlice"
-import posts from "../modules/postSlice2"
 
 // 스토어 연결
 const store = configureStore({
   reducer: {
     // 로그인, 회원가입용
     user,
-
     // 작성용
     posts,
-
     //마이페이지
     mylist,
-
     // 게시글 상세조회, 삭제
     post,
-
     // 코멘트
     cmt,
-
     // 검색
     search,
-
     // 게시글 전체조회
     list,
   },
--- src/redux/modules/listSlice.js
+++ src/redux/modules/listSlice.js
@@ -1,11 +1,8 @@
-
 // 첫 화면에 나오는 포스트 목록들의 모듈 (GET)
 
-import { createSlice } from "@reduxjs/toolkit";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import Apis from "../../shared/Apis";
+import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit"
 import Apis from "../../shared/Apis"
-
 
 const initialState = {
   list: [
@@ -50,9 +47,9 @@ export const __getTimeList = createAsyncThunk(
     try {
       // console.log("getList payload : ", payload)
       const res = await Apis.getPostTimeAX(payload)
-      return thunkAPI.fulfillWithValue(res.data);
+      return thunkAPI.fulfillWithValue(res.data)
     } catch (error) {
-      return thunkAPI.rejectWithValue(error);
+      return thunkAPI.rejectWithValue(error)
     }
   }
 )
@@ -64,40 +61,38 @@ export const listSlice = createSlice({
   extraReducers: {
     // 게시글 좋아요 수 Sort
     [__getLikeList.pending]: (state) => {
-      state.isLoading = true;
+      state.isLoading = true
     },
     [__getLikeList.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.isSuccess = false;
+      state.isLoading = false
+      state.isSuccess = false
 
       console.log("action.payload", action.payload)
       state.list = action.payload.data
     },
     [__getLikeList.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.isSuccess = false;
-      state.error = action.payload;
+      state.isLoading = false
+      state.isSuccess = false
+      state.error = action.payload
     },
-    
+
     // 게시글 작성시간 순서 Sort
     [__getTimeList.pending]: (state) => {
-      state.isLoading = true;
+      state.isLoading = true
     },
     [__getTimeList.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.isSuccess = false;
+      state.isLoading = false
+      state.isSuccess = false
       console.log("action.payload", action.payload)
-      state.list = action.payload.data;
+      state.list = action.payload.data
     },
     [__getTimeList.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.isSuccess = false;
-      state.error = action.payload;
-
+      state.isLoading = false
+      state.isSuccess = false
+      state.error = action.payload
     },
   },
 })
-
 
 // 액션크리에이터는 컴포넌트에서 사용하기 위해 export 하고
 export const {} = listSlice.actions
